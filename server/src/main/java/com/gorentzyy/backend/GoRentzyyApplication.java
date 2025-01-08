658fb4ce87b09d8caa39d0950877d578ad81f1fc
Jasjeev <jasjeev99@gmail.com>
2025-01-08 22:40:45 +0530

    Add initial server setup with application properties, Maven wrapper, and test configuration

---
+++ server/src/main/java/com/gorentzyy/backend/GoRentzyyApplication.java
@@ -0,0 +1,13 @@
+package com.gorentzyy.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoRentzyyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoRentzyyApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/gorentzyy/backend/GoRentzyyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gorentzyy.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoRentzyyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
