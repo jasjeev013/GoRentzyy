8ac78e03a455b8043c3879191223c7789ae73974
Jasjeev <jasjeev99@gmail.com>
2025-01-12 01:35:09 +0530

    Add API response classes and update application properties for environment variables

---
+++ server/src/main/java/com/gorentzyy/backend/payloads/ApiResponseData.java
@@ -0,0 +1,19 @@
+package com.gorentzyy.backend.payloads;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponseData {
+    String message;
+    boolean result;
+    List<Object> data;
+
+}
