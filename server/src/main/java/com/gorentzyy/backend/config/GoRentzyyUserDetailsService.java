891b99a0331201195e69771c355991da4ff129a1
Jasjeev <jasjeev99@gmail.com>
2025-02-01 02:56:37 +0530

    Implement custom UserDetailsService and enhance security configuration for user authentication

---
+++ server/src/main/java/com/gorentzyy/backend/config/GoRentzyyUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.gorentzyy.backend.config;
+
+import com.gorentzyy.backend.models.User;
+import com.gorentzyy.backend.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GoRentzyyUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public GoRentzyyUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow(() ->
+                new UsernameNotFoundException("User Details not found with user"+username));
+
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(user.getRole().toString()));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPassword(),authorities);
+    }
+}
--- server/src/main/java/com/gorentzyy/backend/config/SecurityConfig.java
+++ server/src/main/java/com/gorentzyy/backend/config/SecurityConfig.java
@@ -1,27 +1,50 @@
 package com.gorentzyy.backend.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/api/user/**").authenticated()
+                .requestMatchers("/api/car/**").authenticated()
+                .requestMatchers("/api/booking/**").authenticated()
+                .requestMatchers("/api/review/**").authenticated()
+                .requestMatchers("/api/promotion/**").authenticated()
+                .requestMatchers("/api/notification/**").authenticated()
+                .requestMatchers("/api/location/**").authenticated()
+                .requestMatchers("/api/test/**").permitAll());
         http.cors(AbstractHttpConfigurer::disable);
         http.csrf(AbstractHttpConfigurer::disable);
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    /*
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource){
+
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+     */
+
+    // Checks if the password is compromised or not
+//    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker(){
+        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
     @Bean
     public PasswordEncoder passwordEncoder(){
