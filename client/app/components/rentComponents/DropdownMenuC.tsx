6a4aa60680075a096e4909c0677c9c416aac9c9b
Jasjeev <jasjeev99@gmail.com>
2025-04-20 16:05:22 +0530

    chore: update Radix UI dependencies and add new packages
    - Updated @radix-ui/react-dropdown-menu to version 2.1.11
    - Updated @radix-ui/react-popover to version 1.1.10
    - Updated @radix-ui/react-slot to version 1.2.0
    - Added date-fns dependency at version ^4.1.0
    - Added react-day-picker dependency at version 8.10.1

---
+++ client/app/components/rentComponents/DropdownMenuC.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React, { useState } from 'react'
+import { Button } from "../../../@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "../../../@/components/ui/dropdown-menu"
+
+const DropdownMenuC = () => {
+    const [position, setPosition] = useState("bottom")
+    return (
+        <>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="outline" className='w-full'>Open</Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56">
+                    <DropdownMenuLabel>Panel Position</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+                        <DropdownMenuRadioItem value="top">Top</DropdownMenuRadioItem>
+                        <DropdownMenuRadioItem value="bottom">Bottom</DropdownMenuRadioItem>
+                        <DropdownMenuRadioItem value="right">Right</DropdownMenuRadioItem>
+                    </DropdownMenuRadioGroup>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </>
+    )
+}
+
+export default DropdownMenuC
--- client/app/components/rentComponents/SearchBarSection.tsx
+++ client/app/components/rentComponents/SearchBarSection.tsx
@@ -1,28 +1,37 @@
+"use client";
 import React from 'react'
 import { FiCalendar, FiClock, FiMapPin } from 'react-icons/fi';
+import DatePicker from './DatePicker';
+import DropdownMenuC from './DropdownMenuC';
+
 const SearchBarSection = () => {
+
     return (
+
+
         <>
-            <div className="bg-purple-600 text-white p-6">
+            <div className=" text-white p-6">
                 <div className="max-w-7xl mx-auto">
                     <h1 className="text-2xl font-bold mb-6">Book Self-Drive Cars</h1>
                     <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
                         <div className="relative">
                             <FiMapPin className="absolute left-3 top-3 text-purple-300" />
-                            <select className="w-full p-3 pl-10 rounded-lg bg-purple-700 text-white">
+                            {/* <select className="w-full p-3 pl-10 rounded-lg bg-purple-700 text-white">
                                 <option>Delhi</option>
                                 <option>Mumbai</option>
                                 <option>Bangalore</option>
-                            </select>
+                            </select> */}
+                            <DropdownMenuC/>
                         </div>
 
                         <div className="relative">
-                            <FiCalendar className="absolute left-3 top-3 text-purple-300" />
-                            <input
+                            {/* <FiCalendar className="absolute left-3 top-3 text-purple-300" /> */}
+                            {/* <input
                                 type="date"
                                 className="w-full p-3 pl-10 rounded-lg bg-purple-700 text-white"
                                 placeholder="Pick-Up Date"
-                            />
+                            /> */}
+                             <DatePicker />
                         </div>
 
                         <div className="relative">
